da6ce6af187e4f4933ff0d7b8efc736562b3768e
Glenn Stovall <glennstovall@gmail.com>
2021-10-01 10:25:30 -0400

    styles, basic structure.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Bridges from '../_data/bridges'
-import styles from '../styles/Home.module.css'
 
 interface TBridge {
   name: string,
@@ -11,19 +10,21 @@ interface TBridge {
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>bridge.quest</title>
         <meta name="description" content="A directory of cross-chain bridges" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="https://cdn.rawgit.com/kimeiga/bahunya/css/bahunya-0.1.3.css"/>
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+      <header>
+        <h1>
           bridge.quest
         </h1>
-        <h2>Coming Soon</h2>
-        <p>A directory of cross-chain bridges</p>
+
+        <p>A directory of cryptocurrency bridges</p>
+      </header>
+      <main>
         <table>
           <thead>
             <tr>
@@ -35,19 +36,22 @@ const Home: NextPage = () => {
             {Bridges
               .sort((a:TBridge, b:TBridge) => a.name < b.name ? -1 : a.name > b.name ? 1 : 0)
               .map((bridge: TBridge) => (
-              <tr>
+              <tr key={bridge.name}>
                 <td key={bridge.name}>
                   <a href={bridge.url}>{bridge.name}</a>
                 </td>
                 <td>
-                  {bridge.networks.join(" , ")}
+                  {bridge.networks.sort().join(" , ")}
                 </td> 
               </tr>   
             ))}
           </tbody>
         </table>
       </main>
-
+      <footer>
+        <a href="https://github.com/GSto/bridge.quest">Github</a>
+        <a href="https://twitter.com/GSto">Twitter</a>
+      </footer>
     </div>
   )
 }
